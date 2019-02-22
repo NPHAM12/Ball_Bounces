1e175d92e7b7145feefc0485bdd3bcf24e838a38
Billy <40702630+NPHAM12@users.noreply.github.com>
2019-02-22 14:29:41 -0800

    Update npham12_ballBounces.c

--- npham12_ballBounces.c
+++ npham12_ballBounces.c
@@ -1,11 +1,6 @@
 /* Ball Bounces
    Billy Pham*/
 
-/*Write a program that simulates ball bounces
- Use a integer array to represent the slots
- The program should ask user to input the number of balls, run 10 iterations and print out the result
- Also be careful with the boundaries*/
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
